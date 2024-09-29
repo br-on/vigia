821e7783d51f5830dda1c57770e20e9d3ba1a6e8
bruno <114887935+010101z@users.noreply.github.com>
2024-09-29 14:06:27 -0300

    colors.js

---
+++ assets/script/colors.js
@@ -0,0 +1,11 @@
+const coresPorGrupo = {
+    neurologicos: "#88490D", 
+    respiratorios: "#26B63E", 
+    digestivos: "#060270", 
+    musculoesqueleticos: "#632E2E",
+    "cardiacos-circulatorios": "#D20103", 
+    dermatologicos: "#C63DE9", 
+    "infecciosos-febris": "#4419AA",
+    "ginecologicos-congenitos": "#3B0A49",
+    "sistemicos-gerais": "#2B5D9C" 
+};
